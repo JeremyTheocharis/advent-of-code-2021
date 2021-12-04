60e5242cb9f3889abb129e8557275a0057a1e597
JeremyTheocharis <jt@umh.app>
2021-12-04 13:52:58 +0100

    feat: basic construct of day1

---
+++ day1/target/debug/deps/day1-f6bf04934a365449.d
@@ -0,0 +1,5 @@
+/home/jeremy/git/advent-of-code-2021/day1/target/debug/deps/day1-f6bf04934a365449: src/main.rs
+
+/home/jeremy/git/advent-of-code-2021/day1/target/debug/deps/day1-f6bf04934a365449.d: src/main.rs
+
+src/main.rs:
